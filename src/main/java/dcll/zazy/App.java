728e0af51751c399cef24525e0f1b6996e477fd7
Abel <krusnik02@hotmail.fr>
2015-03-02 11:33:38 +0100

    Initial Commit

---
+++ src/main/java/dcll/zazy/App.java
@@ -0,0 +1,13 @@
+package dcll.zazy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
